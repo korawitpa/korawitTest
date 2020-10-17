bb27abf891e4344de579b39892ec428a3a7d6923
Administrator <admin@example.com>
2020-10-17 22:04:19 +0700

    Add call config and limit file size 50MB

--- Backend/App/__init__.py
+++ Backend/App/__init__.py
@@ -1,5 +1,26 @@
 from flask import Flask
 from App.Upload import app as upload_module
 
+from yaml import load, dump
+try:
+    from yaml import CLoader as Loader, CDumper as Dumper
+except ImportError:
+    from yaml import Loader, Dumper
+
+# Define constant
+CONFIG_FILE = 'Config.yaml'
+
+# Load configuration
+try:
+    FileStream = open(CONFIG_FILE, "r")
+    # Load configuration into config
+    Config = load(FileStream, Loader=Loader)
+except Exception as e:
+    print("Read configuration file error:", e)
+    exit(1)
+
+
 app = Flask(__name__)
+app.config['MAX_CONTENT_LENGTH'] = Config['file_size'] * 1024 * 1024  # Mb unit
+
 app.register_blueprint(upload_module, url_prefix='/upload')
