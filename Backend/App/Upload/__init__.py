1fd9b8707d1c90565c3fb938af3ad3883a869ad4
Administrator <admin@example.com>
2020-10-17 22:05:35 +0700

    Add url Upload and Get file

--- Backend/App/Upload/__init__.py
+++ Backend/App/Upload/__init__.py
@@ -1,14 +1,37 @@
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify, request, send_file, send_from_directory
 import datetime
+import os
+from werkzeug.utils import secure_filename
+from Database import Database
+
+from yaml import load, dump
+try:
+    from yaml import CLoader as Loader, CDumper as Dumper
+except ImportError:
+    from yaml import Loader, Dumper
+
+# Define constant
+CONFIG_FILE = 'Config.yaml'
+
+# Load configuration
+try:
+    FileStream = open(CONFIG_FILE, "r")
+    # Load configuration into config
+    Config = load(FileStream, Loader=Loader)
+except Exception as e:
+    print("Read configuration file error:", e)
+    exit(1)
+
 
 # Blueprint
 app = Blueprint('upload', __name__)
 
-# # init database
-# database = Database.Database(Config['mysql']['database'])
+# init database
+database = Database()
 
-sym_warning = u'🚸'
-sym_correct = u'✅'
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in Config['allowed_extensions']
 
 
 # Flask Module allow origin
@@ -20,6 +43,53 @@ def after_request(response):
     return response
 
 
-@app.route('', methods=['GET'])
-def RootEndpoint():
-    return jsonify({'message': 'welcome to upload module'})
+# @app.route('', methods=['GET'])
+# def RootEndpoint():
+#     return jsonify({'message': 'welcome to upload module'})
+
+
+@app.route('', methods=['POST'])
+def upload():
+    # check if the post request has the file part
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file key'}), 404
+    file = request.files['file']
+    # if user does not select file, browser also
+    # submit an empty part without filename
+    if file.filename == '':
+        return jsonify({'error': 'No selected file'}), 404
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+
+        upload_data = {}
+        upload_data['FilePath'] = os.path.join(Config['upload_folder'], filename)
+        upload_data['FileName'] = filename
+        upload_data['FileType'] = file.content_type
+
+        # Check file
+        result_status, result_msg = database.getData(upload_data['FileName'])
+        if not result_status:
+            return jsonify({'error': result_msg}), 406
+        if result_msg:
+            return jsonify({'error': 'Duplicate file'}), 406
+
+        # Save file
+        file.save(os.path.join(Config['upload_folder'], upload_data['FileName']))
+
+        with open(os.path.join(Config['upload_folder'], upload_data['FileName']), 'rb') as f:
+            upload_data['FileSize'] = len(f.read())
+
+        # Upload to database
+        result_status, result_msg = database.uploadData(upload_data)
+        if not result_status:
+            return jsonify({'error': result_msg}), 406
+        return jsonify({'msg': 'upload success'})
+    return jsonify({'error': 'File type is unsupported'}), 406
+
+
+@app.route('<string:filename>', methods=['GET'])
+def get(filename):
+    result_status, result_msg = database.getData(filename=filename)
+    if not result_status:
+        return jsonify({'error': result_msg}), 406
+    return send_file(os.path.join(os.getcwd(), result_msg[0]['FilePath']))
