a4fbb16970e668f8ceb4e8218b7a117e133c7d96
Administrator <admin@example.com>
2020-10-20 00:06:45 +0700

    Add form moduel for ngModel

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,22 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './partial-component/header/header.component';
 import { ContentComponent } from './partial-component/content/content.component';
+import { RenameComponent } from './partial-component/rename/rename.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ContentComponent
+    ContentComponent,
+    RenameComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
