3565737aa20447da66306ff1831a45abe5698d11
Administrator <admin@example.com>
2020-10-21 00:08:01 +0700

    Add alert component

---
+++ Frontend/src/app/partial-component/alert/alert.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { PopupService } from '../../service/popup.service'
+
+@Component({
+  selector: 'app-alert',
+  templateUrl: './alert.component.html',
+  styleUrls: ['./alert.component.css']
+})
+export class AlertComponent implements OnInit {
+
+  data = {}
+
+  constructor(
+    private popup_service: PopupService
+  ) {
+    this.popup_service.currentAlertStatus.subscribe(
+      result => {
+        this.data = result
+      }
+    )
+   }
+
+  ngOnInit(): void {
+  }
+
+  onOK() {
+    this.popup_service.openAlert(false,'','')
+  }
+}
