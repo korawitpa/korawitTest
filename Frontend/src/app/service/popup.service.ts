274a93dc34730914f6677394a90782603c9593c6
Administrator <admin@example.com>
2020-10-21 00:10:02 +0700

    Add service delete, loadding, alert and preview

--- Frontend/src/app/service/popup.service.ts
+++ Frontend/src/app/service/popup.service.ts
@@ -21,4 +21,50 @@ export class PopupService {
   openRename(status: boolean, fileURL:string, fileID: number, fileOldname: string, fileNewname: string, fileType: string) {
     this.renameStatus.next({status: status, fileURL: fileURL, fileID: fileID, fileOldname: fileOldname, fileNewname: fileNewname, fileType: fileType})
   }
+
+  private deleteStatus = new BehaviorSubject({
+    status: false,
+    id: 0,
+    file_name: '',
+    file_path: '',
+    file_thumbnail_path: ''
+  });
+  currentDeleteStatus = this.deleteStatus.asObservable();
+
+  openDelete(status: boolean, fileID: number, fileName: string, filePath: string, fileThumbnailPath: string) {
+    this.deleteStatus.next({status: status, id: fileID, file_name: fileName, file_path: filePath, file_thumbnail_path: fileThumbnailPath})
+  }
+
+  private loadingStatus = new BehaviorSubject(false);
+  currentLoadingStatus = this.loadingStatus.asObservable();
+
+  openLoading(status: boolean) {
+    this.loadingStatus.next(status)
+  }
+
+  private alertStatus = new BehaviorSubject(
+    {
+      status: false,
+      type: '',
+      message: ''
+    }
+  );
+  currentAlertStatus = this.alertStatus.asObservable();
+
+  openAlert(status: boolean, type: string, message: string) {
+    this.alertStatus.next({status:status, type:type, message:message})
+  }
+
+  private previewStatus = new BehaviorSubject(
+    {
+      status: false,
+      type: '',
+      url: ''
+    }
+  );
+  currentPreviewStatus = this.previewStatus.asObservable();
+
+  openPreview(status: boolean, type: string, url: string) {
+    this.previewStatus.next({status:status, type:type, url:url})
+  }
 }
