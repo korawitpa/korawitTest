f5f12a9593cc91a5bbcd395b909a66735750bad4
Administrator <admin@example.com>
2020-10-21 00:10:21 +0700

    Add filter

--- Frontend/src/app/service/rest-api.service.ts
+++ Frontend/src/app/service/rest-api.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 
@@ -13,8 +13,9 @@ export class RestAPIService {
   
   private URL= 'http://127.0.0.1:8000/upload'
   
-  getData(): Observable<any> {
-    return this.http.get(this.URL +'/database')
+  getData(filename:string, filetype:string, uploaddate:string): Observable<any> {
+    let params = new HttpParams().set('filename',filename).set('filetype', filetype).set('uploaddate', uploaddate)
+    return this.http.get(this.URL +'/database', {params: params})
   }
   uploadData(selectFile: File): Observable<any> {
     let formData: FormData = new FormData();
@@ -22,7 +23,10 @@ export class RestAPIService {
     return this.http.post(this.URL, formData)
   }
   renameData(fileID:number, oldName:string, newName: string): Observable<any> {
-    console.log({id: fileID, old_filename: oldName, new_filename: newName})
     return this.http.put(this.URL, {id: fileID, old_filename: oldName, new_filename: newName})
+  }
+  deleteData(fileID:number, filePath:string, fileThumbnailPath:string): Observable<any> {
+    let params = new HttpParams().set('id',fileID.toString()).set('file_path', filePath).set('file_thumbnail_path', fileThumbnailPath)
+    return this.http.delete(this.URL, {params: params})
   }
 }
