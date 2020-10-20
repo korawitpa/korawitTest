74b6bf98ead7592720f1865495618ce2487aa9fc
Administrator <admin@example.com>
2020-10-21 00:09:18 +0700

    Change filter filetype to drowdown

--- Frontend/src/app/partial-component/content/content.component.ts
+++ Frontend/src/app/partial-component/content/content.component.ts
@@ -18,35 +18,71 @@ export class ContentComponent implements OnInit {
   }
 
   images: []
+  filter: {} = {
+    filename: '',
+    filetype: '',
+    uploaddate: ''
+  }
+
   getdatabase() {
-    this.apiservice.getData().subscribe(
+    this.popup_service.openLoading(true)
+    if (this.filter['filetype'] != ''){
+      if (this.filter['filetype'] == 'mp4'){
+        this.filter['filetype'] = 'video/' + this.filter['filetype']
+      }
+      else {
+        this.filter['filetype'] = 'image/' + this.filter['filetype']
+      }
+    }
+    this.apiservice.getData(this.filter['filename'], this.filter['filetype'], this.filter['uploaddate']).subscribe(
       result => {
+        this.popup_service.openLoading(false)
         result['msg'].forEach(element => {
           element['URL'] = 'http://127.0.0.1:8000/upload/' + element['FileName'] + '/thumbnail'
-          console.log(element['URL'])
         });
         this.images = result['msg']
-        console.log(this.images)
       },
       error => {
-        console.log(error)
+        this.popup_service.openLoading(false)
+        if (error.status == 0){
+          this.popup_service.openAlert(true, 'alert', "Can't connect API please check")
+        }
+        else{
+          this.popup_service.openAlert(true, 'alert', error.error['error'])
+        }
       }
     )
   }
   upload(event){
+    this.popup_service.openLoading(true)
     if (event.target.files.length > 0){
       this.apiservice.uploadData(event.target.files[0]).subscribe(
         result => {
-          console.log(result)
+          this.popup_service.openLoading(false)
+          this.getdatabase()
+          // this.popup_service.openAlert(true, 'correct', result['msg'])
         },
         error => {
+          this.popup_service.openLoading(false)
           console.log(error)
+          if (error.status == 406){
+            this.popup_service.openAlert(true, 'alert', error.error['error'])
+          }
+          else{
+            this.popup_service.openAlert(true, 'alert', 'Check your file do not excess 50MB')
+          }
+          
         }
       )
     }
   }
   onRename(file){
-    console.log(file)
     this.popup_service.openRename(true, file['URL'].replace('/thumbnail', ''), file['ID'], file['FilePath'], '', file['FileType'])
+  }
+  onDelete(file){
+    this.popup_service.openDelete(true, file['ID'], file['FileName'], file['FilePath'], file['FileThumbnailPath'])
+  }
+  onPreview(file){
+    this.popup_service.openPreview(true, file['FileType'], file['URL'].replace('/thumbnail', ''))
   }
 }
